bc590b3589f7720980a51f2682f93b3ee5b5caf1
Vikas Kaushik <vikaskaushik00008@gmail.com>
2022-07-16 22:48:11 +0530

    Add files via upload
    Project on blog

---
+++ my_site/blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(),
+         name="post-detail-page"),  # posts/my-first-post
+    path("read-later", views.ReadLaterView.as_view(), name = "read-later"),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
